e377762c5be1f4b3691774e02f902f5368a5aa92
Emiliano A. Billi <emiliano.billi@gmail.com>
2024-09-28 14:19:39 -0300

    Update ulandrv.c

--- lkm/ulandrv.c
+++ lkm/ulandrv.c
@@ -313,7 +313,8 @@ static ssize_t ulan_io_write(struct file *filp, const char __user *ubuf, size_t 
         dev_kfree_skb(skb);
         return -EFAULT;
     }
-    skb->csum = CHECKSUM_PARTIAL;
+    //skb->csum = CHECKSUM_PARTIAL;
+    skb->ip_summed = CHECKSUM_UNNECESSARY;
     skb->mac_len = 0;
     skb->len = count;
     // Establecer que estamos manejando un paquete IP
